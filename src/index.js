cedb3ef6b2832e9209798fb367857ecfab2ed817
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-28 02:59:09 -0700

    Restructure, allow paddle movement w/keydowns

--- src/index.js
+++ src/index.js
@@ -17,44 +17,86 @@
 
 const gameBoard = document.getElementById("canvas");
 const drawButton = document.getElementById("draw-button");
-
 const context = gameBoard.getContext("2d");
 
-drawButton.addEventListener("click", animateBall);
+drawButton.addEventListener("click", startGame);
+document.addEventListener("keydown", handleKeydown);
 
 context.save();
 
-// context.moveTo(0, 0);
-// context.lineTo(500, 500);
-// context.stroke();
+const pongBall = new GamePiece(250, 250, "ball");
+const player1Paddle = new GamePiece(233, 480, "paddle");
+const player2Paddle = new GamePiece(233, 20, "paddle");
 
-// context.moveTo(500, 0);
-// context.lineTo(0, 500);
-// context.stroke();
+function createGame(gameCode) {
+  const player1 = new Player("Jon", 1, player1Paddle);
+  const player2 = new Player("Joe", 2, player2Paddle);
+  const newGame = new Game(player1, player2, pongBall, gameCode);
+  return newGame;
+}
 
-context.moveTo(250, 250);
-context.arc(250, 250, 10, 180, 360);
-context.stroke();
+function startGame() {
+  const gameData = createGame(Math.random);
+  console.log(gameData);
+  drawGamePiece(gameData.pongBall);
+  drawGamePiece(gameData.player1.paddle);
+  drawGamePiece(gameData.player2.paddle);
+  animateBall();
+}
+
+// context.moveTo(250, 250);
+// context.arc(250, 250, 10, 180, 360);
+// context.stroke();
 
 function drawNextBall(prevX, prevY, dx, dy) {
+  context.beginPath();
   context.arc(prevX + dx, prevY + dy, 10, 180, 360);
   context.stroke();
 }
 
-function erasePreviousBall() {
-  context.beginPath();
+function eraseCanvas() {
+  // context.beginPath();
   // context.restore();
   context.clearRect(0, 0, 500, 500);
 }
 
-// requestAnimationFrame(() => animateBall());
-
 function animateBall() {
-  // erasePreviousBall();
   for (let i = 250; i < 480; i++) {
-    requestAnimationFrame(() => erasePreviousBall());
-    requestAnimationFrame(() => drawNextBall(i, i, 1, 1));
+    setInterval(() => requestAnimationFrame(() => eraseCanvas()), i * 20);
+
+    setInterval(
+      () => requestAnimationFrame(() => drawNextBall(i, i, 1, 1)),
+      i * 20
+    );
   }
 }
 
-const pongBall = new GamePiece();
+function drawGamePiece(gamePiece) {
+  context.moveTo(gamePiece.x, gamePiece.y);
+  if (gamePiece.type === "ball") {
+    context.arc(gamePiece.x, gamePiece.y, 10, 180, 360);
+  } else if (gamePiece.type === "paddle") {
+    context.rect(gamePiece.x, gamePiece.y, 35, 10);
+  }
+  context.stroke();
+}
+
+function movePaddle(dx, playerPaddle) {
+  context.beginPath();
+  eraseCanvas();
+  player1Paddle.x += dx;
+  drawGamePiece(playerPaddle);
+}
+
+function handleKeydown(event) {
+  const key = event.code;
+  if (key === "ArrowLeft") {
+    movePaddle(-1, player1Paddle);
+  } else if (key === "ArrowRight") {
+    movePaddle(1, player1Paddle);
+  } else if (key === "z") {
+    movePaddle(-1, player2Paddle);
+  } else if (key === "x") {
+    movePaddle(1, player2Paddle);
+  }
+}
